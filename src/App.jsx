ae2780bc980a200a95b50afc5d0e6a00a30c9774
Andres_Prado_Morgaz <andres.prado.external@eviden.com>
2024-08-16 14:42:02 +0200

    Some styles

--- src/App.jsx
+++ src/App.jsx
@@ -4,7 +4,7 @@ import { MoviesList } from './components/MoviesList/MoviesList'
 
 function App () {
   return (
-    <div style={{ padding: '15px' }}>
+    <div style={{ padding: '15px', display: 'flex', flexDirection: 'column', gap: '1rem', alignItems: 'center' }}>
       <h1>TV Tracker Project</h1>
       <MoviesList movies={movies} />
     </div>
--- src/components/MoviesList/MoviesList.jsx
+++ src/components/MoviesList/MoviesList.jsx
@@ -1,10 +1,14 @@
+import './MoviesList.css'
+
 export const MoviesList = ({ movies }) => {
   return (
-    <ul>
+    <ul className='moviesList'>
       {movies.map(movie =>
         <li key={movie.imdbID}>
-          {movie.Title}
-          <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          <p>{movie.Title}</p>
+          <div className='moviesList--imgContainer'>
+            <img src={movie.Poster} alt={`${movie.Title} poster`} />
+          </div>
         </li>
       )}
     </ul>
