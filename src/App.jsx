7cd2d78e0a6989d6b7a314ad529935627f86682c
Andres_Prado_Morgaz <andres.prado.external@eviden.com>
2024-08-17 17:52:24 +0200

    Mini-refactor

--- src/App.jsx
+++ src/App.jsx
@@ -32,24 +32,31 @@ function App () {
   return (
     <div className='app'>
       <h1>TV Tracker</h1>
-      <header>
-        <form onSubmit={handleSubmit} className='searchForm' style={{ display: 'flex', justifyContent: 'center', gap: '1rem', border: '1px solid black', padding: '0.5rem', borderRadius: '8px', boxShadow: '0 0 5px gray' }}>
-          <label htmlFor='movieTitle'>
-            <input value={search} onChange={handleChange} type='text' name='movieTitle' placeholder='Avengers, The Matrix, ...' />
-          </label>
-          <label htmlFor='sortCheckbox'>
-            <input onChange={toggleSort} checked={sort} type='checkbox' name='sortCheckbox' />
-            Order by title
-          </label>
-          <button type='submit'>Search</button>
-        </form>
-        {searchError &&
-          <p style={{ color: 'red' }}>
-            {searchError}
-          </p>}
-      </header>
       <main className='app--main'>
-        {!search ? <></> : loading ? <p>Loading...</p> : <MoviesList movies={movies} />}
+        <section className='app--searchBar'>
+          <form onSubmit={handleSubmit} className='searchForm' style={{ display: 'flex', flexDirection: 'column', alignItems: 'center', gap: '1rem', border: '1px solid black', padding: '0.5rem', borderRadius: '8px', boxShadow: '0 0 5px gray' }}>
+            <fieldset>
+              <label htmlFor='movieTitle'>
+                <input value={search} onChange={handleChange} type='text' name='movieTitle' placeholder='Avengers, The Matrix, ...' />
+              </label>
+              {searchError &&
+                <p style={{ color: 'red' }}>
+                  {searchError}
+                </p>}
+            </fieldset>
+            <fieldset>
+              Filters
+              <label htmlFor='sortCheckbox'>
+                <input onChange={toggleSort} checked={sort} type='checkbox' name='sortCheckbox' />
+                Order by title
+              </label>
+            </fieldset>
+            <button type='submit'>Search</button>
+          </form>
+        </section>
+        <section className='app--results' style={{ width: '100%', display: 'flex', justifyContent: 'center' }}>
+          {!search ? <></> : loading ? <p>Loading...</p> : <MoviesList movies={movies} />}
+        </section>
       </main>
     </div>
   )
